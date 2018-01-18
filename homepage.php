7885556cf3f5d257f870c2e62867fbe710f489b2
BLONDEAU Laura <laura.blondeau@my-digital-school.org>
2018-01-18 14:41:33 +0100

    Fusion des fichiers

--- homepage.php
+++ homepage.php
@@ -1,15 +1,50 @@
 <!doctype html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>Itschool</title>
-		<link rel="icon" href="images/Logo.png">
-	</head>
+<head>
+<meta charset="utf-8">
+<title>Itschool</title>
 
-	<body>
-		<?php
-			include('parts/header.php');
-			include('index.php');
-		?>
+<link rel="icon" href="images/logo.png">
+<link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/homepage.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/eva.css">
+</head>
+
+<body>
+
+<?php
+	include('parts/header.php');
+?>
+
+ <div class="content">
+ 	<div class="block-content">
+ 		<div class="inner">
+			<img src="images/Photo_etudiants.PNG">
+			<div class="method">
+				<div class="text">
+					<h1>Nos méthodes</h1>	
+				</div>
+			<div class="images">
+				<div class="broderie">
+					<p>Broderie</p>
+				</div>
+				<div class="flocage">
+					<p>Flocage</p>
+				</div>
+				<div class="serigraphie">
+					<p>Sérigraphie</p>
+				</div>
+			</div>
+			</div>
+		
+ 	</div>
+ </div>
+
+<?php 
+	include('button.php');
+	include('parts/footer.php');
+?>
+
 	</body>
 </html>
---
+++ parts/footer.php
@@ -0,0 +1,48 @@
+<footer class="block-footer">
+	<div class="inner">
+		<ul>
+			<li><a href="#"> Plan du site </a></li>
+			<li><img src="images/icones/Sans titre - 1.png" alt="Puce" /><a href="#"> Informations </a></li>
+  			<li><img src="images/icones/Sans titre - 1.png" alt="Puce" /><a href="#"> Contact </a></li>
+  			<li><img src="images/icones/Sans titre - 1.png" alt="Puce" /><a href="#"> Mentions légales</a></li>
+  			<li><img src="images/icones/Sans titre - 1.png" alt="Puce" /><a href="#" id="msgpopup"><img src="images/icones/send.png" alt="Send" /> <p>Inscrivez vous à la newsletter</p></a></li>
+		</ul>
+	</div>
+</footer>
+
+	<script type="text/javascript">
+		var lienpopup = document.getElementById('msgpopup');
+		var body = document.body;
+		var popup = document.getElementById('newpopup');
+		
+		lienpopup.addEventListener("click", afficherPopup, false);
+
+		function afficherPopup(){
+			sendQuery('popup.php', body, false);
+		}
+
+		function sendQuery(query, showin, erase){
+			console.log('Function sendQuery');
+			console.log("query : " + query);
+
+			//instance de l'objet
+			xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange=function() {
+				if (this.readyState == 4 && this.status == 200) {
+					console.log("ShowMessage response = ");
+					console.log(this.responseText);
+					if (erase){
+						showin.innerHTML = this.responseText;
+					} else {
+						showin.innerHTML += this.responseText;
+					}
+				}
+			};
+
+			xhttp.open("GET",query , true);
+			xhttp.send();
+		}
+		
+		
+	</script>
+	
