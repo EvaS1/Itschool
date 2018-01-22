f48c5587f6f355e48331a8780487d61202d9cad6
SAINTIER Eva <eva.saintier@my-digital-school.org>
2018-01-22 14:48:12 +0100

    coeur rouge + màj listing

--- listing.php
+++ listing.php
@@ -21,49 +21,70 @@
 		<div class="content">
  			<div class="block-content-listing">
  				<div class="inner">
- 					<div class="location">
- 						<ul>
-							<li>
-								<a href="homepage.php">Accueil </a> > 
-							</li>
-							<li>
-								<a href="#">Vêtements</a> >
-							</li>
-							<li>
-								<a href="#">Sweats</a> 
-							</li> 	
- 						</ul>											
- 					</div>
- 					<div class="display">
- 						<p>Voir :</p>
- 						<img src="images/icones/display.png" alt="display">
- 					</div>
- 					<div class="sort-by">
- 						<form>
- 							<label>Trier par : </label>
-							<select name="sort">
-								<option>Sélectionner
-								<option>Nouveauté
-								<option>Prix croissant
-								<option>Prix décroissant
-							</select>
- 						</form>
- 					</div>
- 					<div class="product1">
- 						<div class="img-product">
- 							<img src="images/listing-1.jpg" alt="image1">
- 						</div>
- 						<div class="first-line">
- 							<p>PARIS</p>
- 							<img src="" alt="heart">
- 						</div>
- 						<div class="description">
- 							<p>Sweat chaud et épais...</p>
- 						</div>
- 						<div class="price">
- 							<p>17,30€</p>
- 						</div>
- 					</div>
+ 					<div class="top">
+						<div class="location">
+							<ul>
+								<li>
+									<a href="homepage.php">Accueil </a> > 
+								</li>
+								<li>
+									<a href="#">Vêtements</a> >
+								</li>
+								<li>
+									<a href="#">Sweats</a> 
+								</li> 	
+							</ul>											
+						</div>
+						<div class="display">
+							<span>Voir :</span>
+							<img src="images/icones/display.png" alt="display">
+						</div>
+						<div class="sort-by">
+							<form>
+								<label>Trier par : </label>
+								<select name="sort">
+									<option>Sélectionner
+									<option>Nouveauté
+									<option>Prix croissant
+									<option>Prix décroissant
+								</select>
+							</form>
+						</div>
+					</div>
+					<div class="products">
+						<div class="product1">
+							<div class="img-product">
+								<img src="images/listing-1.jpg" alt="image1">
+							</div>
+							<div class="first-line">
+								<p>Paris</p>
+								<img src="images/icones/like.png" alt="heart">
+							</div>
+							<div class="description">
+								<p>Sweat unisexe chaud et épais, d’une qualité irréprochable tant sur la qualité du molleton que sur la finition.</p>
+							</div>
+							<div class="price">
+								<p>17,30€</p>
+							</div>
+						</div>
+						<div class="product2">
+							<div class="img-product">
+								<img src="images/listing-2.jpg" alt="image2">
+							</div>
+							<!--<div class="text">-->
+								<div class="first-line">
+									<p>Angers</p>
+									<img src="images/icones/like.png" alt="heart">
+								</div>
+								<div class="description">
+									<p>Le modèle Angers est un sweat à capuche simple et de bonne qualité.</p>
+								</div>
+								<div class="price">
+									<p>18,00€</p>
+								</div>
+							<!--</div>-->
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
